1e6aa68a7e3687a6a7bfa4b07558e73c290b575e
altorresv <atorresv1812@hotmail.com>
2019-07-05 16:04:15 -0500

    Primer commit

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,18 @@
+
+package calculadora;
+
+
+public class Calculadora {
+
+   
+    public static void main(String[] args) {
+            
+        
+    }
+    public static int suma(int a, int b){
+        return a+b;
+    }
+    public static int resta(int a, int b){
+        return a-b;
+    }
+    }
